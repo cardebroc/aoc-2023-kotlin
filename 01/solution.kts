83eb476df8de5cf30f5a347a570a5a4e656821f4
Cariem El Wakil <cariem.elwakil@gmx.de>
2024-12-01 23:29:15 +0100

    solve puzzle one

--- 01/solution.kts
+++ 01/solution.kts
@@ -1,20 +1,32 @@
 import java.io.File
 import java.io.BufferedReader
+import kotlin.math.abs
 
 val bufferedReader: BufferedReader = File("input.txt").bufferedReader()
 val inputString = bufferedReader.use { it.readText() }
 
 val (firstList, secondList) = inputString.split("\n").let { lines ->
-    val firstL = mutableListOf<Pair<String, Int>>()
-    val secondL = mutableListOf<Pair<String, Int>>()
+    val firstL = mutableListOf<Int>()
+    val secondL = mutableListOf<Int>()
 
-    lines.forEachIndexed { index, line ->
-        firstL.add(line.split("   ")[0] to index)
-        secondL.add(line.split("   ")[1] to index)
+    lines.forEach { line ->
+        firstL.add(line.split("   ")[0].toInt())
+        secondL.add(line.split("   ")[1].toInt())
     }
 
     firstL to secondL
 }
 
-println(firstList)
-println(secondList)
+//region part one
+firstList.sortedBy { it }
+    .zip(secondList.sortedBy { it })
+    .sumOf { (firstListElement, secondListElement) ->
+        abs(firstListElement - secondListElement)
+    }
+//endregion
+//region part two
+firstList.sumOf { id ->
+    val factor = secondList.filter { it == id }.size
+    id * factor
+}
+//endregion
