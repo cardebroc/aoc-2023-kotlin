707206c9271cc13b6e37021d2476acf9a7b07ca7
Cariem El Wakil <cariem.elwakil@gmx.de>
2024-12-04 07:43:46 +0100

    solve 03

---
+++ 03/solution.kts
@@ -0,0 +1,28 @@
+import java.io.File
+import java.io.BufferedReader
+import kotlin.math.abs
+
+val bufferedReader: BufferedReader = File("input.txt").bufferedReader()
+val inputString = bufferedReader.use { it.readText() }
+
+val mulRegex = """mul\((\d+),(\d+)\)""".toRegex()
+
+mulRegex.findAll(inputString)
+    .toList()
+    .sumOf { matchResult ->
+        matchResult.groupValues[1].toInt() * matchResult.groupValues[2].toInt()
+    }
+
+val mulRegexAdvanced = """(?:mul\((\d+),(\d+)\))|(?:do\(\))|(?:don't\(\))""".toRegex()
+
+var isActive = true
+mulRegexAdvanced.findAll(inputString)
+    .toList()
+    .sumOf { matchResult ->
+        if (listOf("do()", "don't()").contains(matchResult.value)) {
+            isActive = matchResult.value == "do()"
+        } else {
+            if (isActive) return@sumOf matchResult.groupValues[1].toInt() * matchResult.groupValues[2].toInt()
+        }
+        0
+    }
